c523366b2fe3f6f12cc411d3cca817fe56935328
Prasad Patil <prasadpatil131@gmail.com>
2019-09-27 21:53:52 +0530

    Add files via upload

---
+++ py_file.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+import urllib
+filename = "data_.xlsx"
+file_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00477/Real%20estate%20valuation%20data%20set.xlsx"
+urllib.request.urlretrieve(file_url, filename)
+
+data_xls = pd.read_excel('data_.xlsx')
+data_xls.to_csv('data.csv', index = None)
+data = pd.read_csv('data.csv', index_col = False)
+
+# Transaction date is in float let's make it integer
+
+data["X1 transaction date"]= data["X1 transaction date"].astype(int)
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+X = data.iloc[:,:7]
+X = X.drop(["No"],axis=1)
+y = data.iloc[:,7]
+
+# Modelling
+rr = RandomForestRegressor()
+rr.fit(X,y)
+
+# Saving model with pickle
+pickle.dump(rr, open('pickle_file.pkl','wb'))
+
+model = pickle.load(open('main_model.pkl','rb'))
+print(model.predict([[2013,30,300,3,30,121]]))
